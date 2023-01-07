f1dbb7ea8fa743fd10dbd6370c26841f4a2f1941
almahfuzhossen <115609814+almahfuzhossen@users.noreply.github.com>
2023-01-07 19:14:29 +0600

    2 files modified

--- App.tsx
+++ App.tsx
@@ -2,10 +2,30 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const datas = [
+    {
+      name: 'component1',
+      age: 13,
+    },
+    {
+      name: 'compo-2',
+      age:111
+    },
+    {
+      name: 'compo compo compo- 3',
+      age:111
+    },
+  ];
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <div>
+        <h1>Explore React - Day 1</h1>
+        {datas.map((data) => {
+          console.log(data)
+          return <li>{data.name} </li>;
+          console.log(data);
+        })}
+      </div>
     </div>
   );
 }
---
+++ Component/Home.js
@@ -0,0 +1,5 @@
+import react from 'react';
+
+export default Home = () => {
+  return <h1>this is from home page </h1>;
+};
